5cdf71bac2c1bdac97d16f8cad29df4a2b439e1f
Samuel Olagunju <plexgit@gmail.com>
2022-01-26 00:16:54 -0500

    Removed functions.js and replaced it with HTML file

--- functions/functions.js
+++
@@ -1,8 +0,0 @@
-function favoriteAnimal(animal, isAre) {
-  console.log(animal + isAre + " my favorite animal! :D")
-}
-
-favoriteAnimal("dogs", " are")
-favoriteAnimal("birds", " are")
-favoriteAnimal("luna", " is")
-

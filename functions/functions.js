ec66e9e5bfcec5185a0ccf810f04ee29f8c5f223
Samuel Olagunju <plexgit@gmail.com>
2022-01-26 00:02:36 -0500

    i learned a bit about functions

---
+++ functions/functions.js
@@ -0,0 +1,8 @@
+function favoriteAnimal(animal, isAre) {
+  console.log(animal + isAre + " my favorite animal! :D")
+}
+
+favoriteAnimal("dogs", " are")
+favoriteAnimal("birds", " are")
+favoriteAnimal("luna", " is")
+
